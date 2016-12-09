729b1b9eec869b1beaf44dab71ba0d923f2d78f1
nakame <jiyu1000@gmail.com>
2016-12-09 18:35:44 +0900

    firebase追加

--- webviewapp/webviewapp/AppDelegate.swift
+++ webviewapp/webviewapp/AppDelegate.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016年 nakamehiroya. All rights reserved.
 //
 
+
+import Firebase
 import UIKit
 
 @UIApplicationMain
@@ -15,9 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+   
+        FIRApp.configure()
+        
         return true
     }
+    
+        
+        
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
